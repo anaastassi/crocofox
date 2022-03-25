2897c5d9522f387cdbcfe3f842b782f137f12357
anaastassi <anaastassi@gmail.com>
2022-03-25 17:45:34 +0200

    myfox

--- app.py
+++ app.py
@@ -1,7 +1,24 @@
+import mysql
 from flask import Flask, render_template,url_for,redirect
+# from flask_sqlalchemy import SQLAlchemy
 from flask_bootstrap import Bootstrap
+from flask_mysqldb import MySQL
+import mysql.connector
+
 app = Flask(__name__)
 Bootstrap(app)
+mydb= mysql.connector.connect(
+    host='localhost',
+    user='root',
+    passwd='12345678',
+    database='flaskapp'
+)
+my_cursor= mydb.cursor()
+#
+# app.config['MYSQL_HOST']='localhost'
+# app.config['MYSQL_USER']='root'
+# app.config['MYSQL_PASSWORD']='12345678'
+# app.config['MYSQL_DB']='flaskapp'
 @app.route('/')
 def index():
     # return render_template('index.html')
@@ -22,5 +39,33 @@ def fox():
 @app.route('/croc')
 def croc():
     return render_template('croc.html')
+@app.route('/myfox')
+def myfox():
+    return render_template('myfox.html')
+
+def set():
+    name = 'lovely'
+    email = 'crocodile'
+    my_cursor.execute("INSERT INTO users(name,email) VALUES (%s, %s)",(name,email))
+    mydb.commit()
+
+def get():
+
+
+    my_cursor.execute("SELECT * FROM users")
+
+    myresult = my_cursor.fetchall()
+
+    for x in myresult:
+        print(x)
+
+
+
+
+
+
+
 if __name__ == '__main__':
+    set()
+    get()
     app.run(debug= True)
